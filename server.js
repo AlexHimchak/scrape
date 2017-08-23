e3b0cb8f04e23de6df62b464b39f979874bc9485
Alexander Himchak <alexhimchak@alexanders-mbp.attlocal.net>
2017-08-22 22:03:54 -0700

    Port

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+var PORT = process.env.PORT || 3000;
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
@@ -152,6 +154,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
