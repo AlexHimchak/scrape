e1fd65367d538031bb3baaa795a5c4af749c3b76
Alexander Himchak <alexhimchak@Alexanders-MacBook-Pro.local>
2017-08-22 21:31:03 -0700

    Mongod

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://heroku_hpx3bnfh:fk5p2knqba82hkg46uimom8l9q@ds151433.mlab.com:51433/heroku_hpx3bnfh");
 var db = mongoose.connection;
 
 // Show any mongoose errors
